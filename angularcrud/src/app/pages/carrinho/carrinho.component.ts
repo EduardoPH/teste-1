ef04003e6da8276aa4107bb8291223a3c0ed66e0
Alvesisac <141432991+Alvesisac@users.noreply.github.com>
2025-05-18 23:05:53 -0300

    Alteracao - 18/05/2025: consertado logica entre passagem das telas de navegacao.

--- angularcrud/src/app/pages/carrinho/carrinho.component.ts
+++ angularcrud/src/app/pages/carrinho/carrinho.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MenuComponent } from '../../components/menu/menu.component';
+
 
 @Component({
   selector: 'app-carrinho',
   standalone: true,
-  imports: [],
+  imports: [MenuComponent],
   templateUrl: './carrinho.component.html',
   styleUrl: './carrinho.component.css'
 })
--- angularcrud/src/app/pages/home/home.component.ts
+++ angularcrud/src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../shared/header/header.component";
 import { MenuComponent } from "../../components/menu/menu.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, MenuComponent],
+  imports: [HeaderComponent, MenuComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
